a2f4acada7a5c72ebada54c59b6bd478f2b7b093
Xike <zekewang918@gmail.com>
2014-10-23 21:09:26 -0300

    Fix #1: Create Code Structure

--- JXYShell.c
+++ JXYShell.c
@@ -2,13 +2,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define failure 1
+#define success 0
+
 int 
 main(int argc, char *ar[])
 {
+  char* cmdLine;
+  char* path = "/bin/";
   while(1){
-    
+    printf("JXYShell: ");
+    fgets(buffer, 512, stdin);
+    int rc = fork();
+    if (rc < 0){
+      fprintf(stderr, "Fork Failed\n");
+    }else if (rc == 0){
+    }else{
+      Wait(NULL);
+    }
 
   }
+  return 0;
 }
 void piping(){
 
