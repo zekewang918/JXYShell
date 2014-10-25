e2a2567aa06e5e3fdb150c15e4621a58d1ae15c2
Xike <zekewang918@gmail.com>
2014-10-25 01:38:21 -0300

    First Version of Piping

--- JXYShell.c
+++ JXYShell.c
@@ -9,7 +9,12 @@
 #define FAILURE 1
 #define SUCCESS 0
 
+#define COMMAND_LENGTH 512
+#define MAX_COMMEND 512
+
 void executeCommand(char* cmd);
+char* divideString(char* cmd);
+struct commend piping(char* cmd);
 
 int 
 main(int argc, char *argv[])
@@ -24,7 +29,7 @@ main(int argc, char *argv[])
     int rc = fork();
     if (!strcmp(cmdLine, "exit")){
      exit(SUCCESS);
-  }
+    }
     if (rc < 0){
       fprintf(stderr, "Fork Failed\n");
       exit(FAILURE);
@@ -39,16 +44,33 @@ main(int argc, char *argv[])
   return SUCCESS;
 }
 void executeCommand(char * cmd){
-  char *commend = strtok(cmd," ");
+  //char** commend = piping(cmd);
   char* arg[] = {cmd, NULL};
   execvp(arg[0],arg);
   printf("%s\n", cmd);
-  system(cmd);
+  //system(cmd);
 }
 
+/*char* divideString(char* cmd){
 
-void piping(){
+}*/
+struct commend
+{
+  char cmd[MAX_COMMEND][COMMAND_LENGTH+1];
+}commend_line;
 
+struct commend piping(char* cmd){
+  //char commend[MAX_COMMEND][COMMAND_LENGTH+1];
+  char divide[2] = "|";
+  char* token;
+  int index = 0;
+  token = strtok(cmd, divide);
+  while(token != NULL){
+    strcpy(commend_line.cmd[index], token);
+    printf("%s", commend_line.cmd[index]);
+    index++;
+  }
+  return commend_line;
 }
 
 void tabCompletion(){
