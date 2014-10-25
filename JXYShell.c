de7c544e30ebefaedc2713b6834b89a8da06a7f0
Xike <zekewang918@gmail.com>
2014-10-25 13:51:34 -0300

    Make Code Clear

--- JXYShell.c
+++ JXYShell.c
@@ -18,22 +18,15 @@ int executeCommand(char* cmd);
 char* divideString(char* cmd);
 int piping(char* cmd);
 
-struct commend
-{
-  char cmd[MAX_COMMEND][COMMAND_LENGTH+1];
-}commend_line;
-
 int 
 main(int argc, char *argv[])
 {
   char* cmdLine;
   int stop = 0;
-  //char buff[PATH_MAX + 1];
-  //char* cwd = getcwd(buff, PATH_MAX+  1);
+
   while(!stop){
     printf("JXYShell$ -");
     cmdLine = readline("> ");
-    //char* const args[] = {cmdLine,NULL};
     printf("123");
     if (!strcmp(cmdLine, "exit")){
       exit(SUCCESS);
@@ -47,12 +40,8 @@ main(int argc, char *argv[])
         exit(FAILURE);
       }else if (rc == 0){
         printf("IM CHILD");
-        //system(cmdLine);
-        //char *myargs[3];
-        //myargs[0] = strd
         char* arg[] = {strdup("ls"), NULL};
         execvp(arg[0],arg);
-        //executeCommand(cmdLine);
       }else{
         int wc = wait(NULL);
         printf("IM DADDY%d", wc);
@@ -62,29 +51,15 @@ main(int argc, char *argv[])
   return SUCCESS;
 }
 int executeCommand(char * cmd){
-  printf("CMD:");
-
   int num = piping(cmd);
-  printf("NUMBER: %d",num);
   int i;
   for (i = 0;i<num;i++){
-    //printf("%s", commend_line.cmd[i]);
     if (system(commend_line.cmd[i]) == -1){
       return 0;
     }
   }
-  //char* arg[] = {cmd, NULL};
-  //execvp(arg[0],arg);
-  //printf("%s\n", cmd);
-  //system(cmd);
-  //exit(FAILURE);
   return 1;
 }
-
-/*char* divideString(char* cmd){
-
-}*/
-
 
 int piping(char* cmd){
   char divide[2] = "|";
@@ -93,7 +68,6 @@ int piping(char* cmd){
   token = strtok(cmd, divide);
   while(token != NULL){
     strcpy(commend_line.cmd[index], token);
-    printf("%s", commend_line.cmd[index]);
     index++;
     token = strtok(NULL, divide);
   }
