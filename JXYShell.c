3a4b53ad31f955d6235fb2778e39b266e3664c1c
Xike <zekewang918@gmail.com>
2014-10-28 02:07:25 -0300

    Fix #3 and Fix #6: Add Necessary Comments

--- JXYShell.c
+++ JXYShell.c
@@ -1,12 +1,12 @@
-/* * * * * * * * * * * * * * * 
- * JXY SHELL                 *
- *                           *
- * A Simple Shell            *
- *                           *
- * Authors: Jingjing Tang    *
- *          Xike Wang        *
- *          Yang Yang        *
- *****************************/
+/*-----------------------------------------------------*
+ * JXY SHELL                                           *
+ *                                                     *
+ * A Simple Shell                                      *
+ *                                                     *
+ * Authors: Jingjing Tang                              *
+ *          Xike Wang                                  *
+ *          Yang Yang                                  *
+ *******************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,16 +16,39 @@
 #include <readline/history.h>
 #include <sys/wait.h>
 
+/*
+ * Define Staus of exit program
+ * FAILURE - 1   
+ * SUCCESS 0
+ */
 
 #define FAILURE 1
 #define SUCCESS 0
+
+/*
+ * Define the length of command, max number of command, and 
+ * history's size
+ * Length of command - 16
+ * Max number of command - 64
+ * History's size - 100
+ */
+
 
 #define COMMAND_LENGTH 16
 #define MAX_COMMEND 64
 #define HISTORY_MAX_SIZE 100
 
+/*
+ * Create a static char array that stores history
+ * Create a int that stores the number of history 
+ */
+
 static char* history_list[HISTORY_MAX_SIZE];
 static int history_count = 0;
+
+/*
+ * Define that functions includes in this program
+ */
 
 void executeCommand(int num);
 int piping(char* cmd);
@@ -34,26 +57,18 @@ void printHistory();
 void parse(char* line, char** argc);
 void isBuiltIn(char* cmd);
 
+/*
+ * Struct a 2D-array that stores the commands
+ */
 
-int 
-main(void)
-{
-  char* cmdLine;
-  while(1){
-    
-    printf("JXYShell$ -");
-    cmdLine = readline("> ");
-    history(cmdLine);
-    isBuiltIn(cmdLine);
-    executeCommand(piping(cmdLine));
-
-  }
-  return SUCCESS;
-}
 struct commend
 {
   char cmd[MAX_COMMEND][COMMAND_LENGTH+1];
 }commend_line;
+
+/*
+ * Function that execute program by using fork() and execvp()
+ */
 
 void executeCommand(int num){
   int i;
@@ -75,6 +90,10 @@ void executeCommand(int num){
       }
 }
 
+/*
+ * Function that implement piping by identify | signal
+ */
+
 int piping(char* cmd){
   char divide[2] = "|";
   char* token;
@@ -88,6 +107,9 @@ int piping(char* cmd){
   return index;
 }
 
+/*
+ * Function that does parsing job
+ */
 
 void  parse(char *line, char **argv)
 {
@@ -102,7 +124,9 @@ void  parse(char *line, char **argv)
      *argv = (char *) '\0';                
 }
 
-
+/*
+ * Function that prints the history list
+ */
 
 void printHistory(){
   int i;
@@ -110,6 +134,10 @@ void printHistory(){
     printf("%d\t%s\n", i, history_list[i]);
   }
 }
+
+/* 
+ * Function that stores history of user types
+ */
 
 void history(char* command){
   int i;
@@ -125,11 +153,39 @@ void history(char* command){
     history_list[HISTORY_MAX_SIZE-1] = strdup(command);
   }
 }
+
+/*
+ * Checks if commands that user types is built in
+ * If it does then run it by functions provided in this program
+ */
+
 void isBuiltIn(char* cmd){
   if (strcmp(cmd, "history") == 0){
     printHistory();
   }else if (strcmp(cmd, "exit") == 0){
     exit(SUCCESS);
   }
+}
+
+/*
+ * Main Function
+ * Function that simulate a shell that could do
+ * simple jobs
+ */
+
+int 
+main(void)
+{
+  char* cmdLine;
+  while(1){
+    
+    printf("JXYShell$ -");
+    cmdLine = readline("> ");
+    history(cmdLine);
+    isBuiltIn(cmdLine);
+    executeCommand(piping(cmdLine));
+
+  }
+  return SUCCESS;
 }
 
